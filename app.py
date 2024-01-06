f86869d2ea102253ae8e837f206f5359fb29679e
Bayhaqy <achmadbayhaqy92@gmail.com>
2024-01-06 12:32:24 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='Classification - News Analysis and Prediction',  layout='wide', page_icon='📃')
+st.title("📃 Classification - News Analysis and Prediction")
+st.write(
+  """
+  Welcome to the **📃 Classification - News Analysis and Prediction App**!
+  """
+  )
